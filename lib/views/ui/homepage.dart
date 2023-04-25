df42fe1059cf573088f8037a898d70dd49b93a22
Ezinne Nnabugwu <ezinne.blessing@yahoo.com>
2023-04-25 11:21:25 +0100

    first commit

---
+++ lib/views/ui/homepage.dart
@@ -0,0 +1,181 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:online_shop/views/shared/appstyle.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late final TabController _tabController =
+      TabController(length: 3, vsync: this);
+  // this; the home state wouyld pass the vysnc object to the controller
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(16, 45, 0, 0),
+              height: MediaQuery.of(context).size.height * 0.4,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                        'assets/images/top_image.png',
+                      ),
+                      fit: BoxFit.fill)),
+              child: Container(
+                padding: EdgeInsets.only(left: 8, bottom: 15),
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Athletics Shoes',
+                      style: appstyleWithHt(
+                          42, Colors.white, FontWeight.bold, 1.5),
+                    ),
+                    Text(
+                      'Collection',
+                      style: appstyleWithHt(
+                          42, Colors.white, FontWeight.bold, 1.2),
+                    ),
+                    TabBar(
+                        indicatorSize: TabBarIndicatorSize.label,
+                        indicatorColor: Colors.transparent,
+                        controller: _tabController,
+                        isScrollable: true,
+                        labelColor: Colors.white,
+                        labelStyle: appstyle(24, Colors.white, FontWeight.bold),
+                        unselectedLabelColor: Colors.grey.withOpacity(0.3),
+                        tabs: const [
+                          Tab(
+                            text: 'Men Shoes',
+                          ),
+                          Tab(
+                            text: 'Women Shoes',
+                          ),
+                          Tab(
+                            text: 'Kids Shoes',
+                          )
+                        ]),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.265),
+              child: Container(
+                padding: EdgeInsets.only(
+                  left: 12,
+                ),
+                child: TabBarView(controller: _tabController, children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.405,
+                        // color: Colors.amber,
+                        child: ListView.builder(
+                          itemCount: 6,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                color: Colors.grey,
+                                height: MediaQuery.of(context).size.height,
+                                width: MediaQuery.of(context).size.width * 0.6,
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(12, 20, 12, 20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Latest Shoes',
+                                  style: appstyle(
+                                      24, Colors.black, FontWeight.bold),
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      'Show All',
+                                      style: appstyle(
+                                          22, Colors.black, FontWeight.w500),
+                                    ),
+                                    Icon(
+                                      AntDesign.caretright,
+                                      size: 20,
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.13,
+                            child: ListView.builder(
+                              itemCount: 6,
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        color: Colors.grey,
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(16))),
+                                    height: MediaQuery.of(context).size.height *
+                                        0.12,
+                                    width: MediaQuery.of(context).size.width *
+                                        0.28,
+                                    child: CachedNetworkImage(
+                                        imageUrl:
+                                            "https://d326fntlu7tb1e.cloudfront.net/uploads/58282ea3-b815-4d26-9f4f-382aa62f67cf-HP5404_a1.webp"),
+                                  ),
+                                );
+                              },
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.405,
+                        color: Colors.green,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.405,
+                        color: Colors.amber,
+                      )
+                    ],
+                  )
+                ]),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/ui/mainscreen.dart
@@ -0,0 +1,39 @@
+import 'package:community_material_icon/community_material_icon.dart';
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:online_shop/controllers/mainscreen_provider.dart';
+import 'package:online_shop/views/shared/appstyle.dart';
+import 'package:online_shop/views/ui/cartpage.dart';
+import 'package:online_shop/views/ui/homepage.dart';
+import 'package:online_shop/views/ui/profile.dart';
+import 'package:online_shop/views/ui/searchpage.dart';
+import 'package:provider/provider.dart';
+
+import '../shared/bottom_nav.dart';
+import '../shared/bottom_nav_widget.dart';
+
+class MainScreen extends StatelessWidget {
+  MainScreen({super.key});
+
+  List<Widget> pageList = [
+    const HomePage(),
+    const SearchPage(),
+    const HomePage(),
+    const CartPage(),
+    const ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // int pageIndex = 0;
+    return Consumer<MainScreenNotifier>(
+        builder: (context, mainScreenNotifier, child) {
+      return Scaffold(
+        backgroundColor: const Color(0XFFE2E2E2),
+        body: pageList[mainScreenNotifier.pageIndex],
+        bottomNavigationBar: BottomNav(),
+      );
+    });
+  }
+}
+
